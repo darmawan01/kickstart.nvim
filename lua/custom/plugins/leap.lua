9d7d961c8f60ef7f1cd093563e0dbf11af86e2a8
dayatz <dayat.py@gmail.com>
2026-03-16 10:19:50 +0800

    fix nvim 0.11 compat: telescope master, leap codeberg migration, surround v4

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -1,6 +1,8 @@
 return {
-  'ggandor/leap.nvim',
+  url = 'https://codeberg.org/andyg/leap.nvim',
   config = function()
-    require('leap').create_default_mappings()
-  end
+    vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
+    vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
+    vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
+  end,
 }
